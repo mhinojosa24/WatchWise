cef9d6acec8a2b5c0c6f27dbb590a77ded481dfa
Maximo Hinojosa <maximohinojosa13@gmail.com>
2024-05-30 18:15:08 -0400

    [Update] - initial instantiation

---
+++ WatchWise/WatchWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchWise
+//
+//  Created by Maximo Hinojosa on 5/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
